8e17a0aec972aa6f8192e1f450835b2505ed4f83
Blessing Omomola <blessingomomola@gmail.com>
2025-07-23 21:17:20 -0400

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 Application is ready to be deployed to the develeopment environment
-developement must be reviewed by managers before deployment
+developement must be properly reviewed by managers before deployment
